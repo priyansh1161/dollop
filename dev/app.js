e2bcef022616513bb5d05eee89b165d28e85c106
priyansh gupta <priyanshgupta1161@gmail.com>
2017-02-06 12:59:40 +0530

    xhr support and test added

--- dev/app.js
+++ dev/app.js
@@ -1,7 +1,9 @@
 
 function Dollop() {
     // this function will be helpful when you need to call an async opt one after other in sync
-    var dollop = {};
+    var dollop = function () {
+        
+    };
     dollop.prototype.asyncLoop = function (iterations, data, async, cb) {
         var done = false;
         var index = 0;
@@ -26,5 +28,35 @@ function Dollop() {
         loop.next();
         return loop;
     };
+    dollop.prototype.ajax = function (obj) {
+        var data = obj.data || null;
+        var xhr = new XMLHttpRequest();
+        xhr.open(obj.type, obj.url);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                console.log(xhr.status);
+                if ( xhr.status >= 200 && xhr.status <=209) {
+                    console.log(xhr.responseText);// 'This is the returned text.'
+                    obj.success(xhr.responseText);
+                }
+                else {
+                    console.log('Error: ' + xhr.status); // An error occurred during the request.
+                    obj.error(xhr.status);
+                }
+            }
+        };
+        xhr.send(data);
+    };
+    dollop.prototype.ajax({
+        type : 'GET',
+        url : 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
+        success : function(res){
+            
+        },
+        error : function (xhr) {
+
+        }
+    });
     return dollop;
 }
+Dollop();
